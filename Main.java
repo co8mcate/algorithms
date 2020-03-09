47e251c07ea92561e47cee1f148f6ef0058ece1c
co8mcate <37420994+co8mcate@users.noreply.github.com>
2020-03-08 23:30:20 -0700

    Create a first java repo for COMP 482

---
+++ Main.java
@@ -0,0 +1,14 @@
+/**
+ * Main
+ * 
+ * type "class" to set up a layout of java file type "main" to set up a main
+ * function type "sysout" to fill a line of output statement
+ * 
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Colin!");
+
+    }
+}
