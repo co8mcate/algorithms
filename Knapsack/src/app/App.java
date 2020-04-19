2d3ec3a9f20117d8e39ea3325dbc6316e1c44c0a
co8mcate <37420994+co8mcate@users.noreply.github.com>
2020-04-18 22:28:24 -0700

    Project 4 is done

--- Knapsack/src/app/App.java
+++ Knapsack/src/app/App.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.*;
-import java.util.regex.Pattern;
 
 //Implement the O(n log n) algorithm that solves the Counting Inversions problem from Chapter 5.
 
@@ -13,13 +12,12 @@ public class App {
     public static void main(final String[] args) throws FileNotFoundException {
 
         System.out.println("\n===================================\n");
-        System.out.println("\tCounting Inversions\n");
+        System.out.println("\tKnapsack Problem\n");
         System.out.println("===================================\n");
 
         try {
 
-            String filePath = "input3.txt";
-            ArrayList<Integer> list = new ArrayList<>();
+            String filePath = "input4.txt";
 
             // Create an Object for getting a file
             File myFile = new File(filePath);
@@ -28,29 +26,35 @@ public class App {
             // Validate that you have a file
             System.out.println("Found your file!");
 
-            // Use a delimiter to read characters without whitespace
-            scan.useDelimiter(Pattern.compile(" "));
+            int maxcap;
+            maxcap = Integer.parseInt(scan.nextLine());
 
-            // Parse and store data into ArrayList - O(n)
-            while (scan.hasNextLine()) {
-                list.add(scan.nextInt());
+            String str = scan.nextLine();
+            String[] val = str.split(" ");
+            int num = val.length;
+
+            int[] value = new int[num];
+
+            // parse and store data in array - O(n)
+            for (int i = 0; i < num; i++) {
+                value[i] = Integer.parseInt(val[i]);
+            }
+
+            String str2 = scan.nextLine();
+            String[] wgt = str2.split(" ");
+
+            int[] weight = new int[num];
+
+            // parse and store data in array - O(n)
+            for (int i = 0; i < num; i++) {
+                weight[i] = Integer.parseInt(wgt[i]);
             }
 
             System.out.println();
 
-            System.out.println("A list of numbers: " + list);
+            System.out.println(knapsack(maxcap, weight, value, num));
 
             System.out.println();
-
-            // Create a new array for mergeSort
-            int arr[] = new int[list.size()];
-
-            for (int i = 0; i < list.size(); i++) {
-                arr[i] = list.get(i);
-            }
-
-            // Print a number of counting inversions
-            System.out.println("Number of inversions are " + mergeSort(arr));
 
             scan.close();
         } catch (IOException e) {
@@ -58,71 +62,24 @@ public class App {
         }
     }
 
-    // Driver method of MergeSort
-    static int mergeSort(int arr[]) {
+    static int knapsack(int maxcap, int wt[], int val[], int n) {
 
-        int numArray = arr.length;
+        int[][] knap = new int[n + 1][maxcap + 1];
 
-        int temp[] = new int[numArray];
-
-        return mergeSorts(arr, temp, 0, numArray - 1);
-    }
-
-    // returns the number of inversions in the array - O(n log n)
-    static int mergeSorts(int arr[], int temp[], int left, int right) {
-
-        int mid, count = 0;
-
-        if (right > left) {
-
-            // Divide and insert an integer as middle
-            mid = (right + left) / 2;
-
-            count = mergeSorts(arr, temp, left, mid);
-            count += mergeSorts(arr, temp, mid + 1, right);
-
-            // Merge two parts
-            count += merge(arr, temp, left, mid + 1, right);
-        }
-
-        return count;
-    }
-
-    // This method merges two sorted arrays and returns inversion count in the
-    // arrays.
-    static int merge(int arr[], int temp[], int left, int mid, int right) {
-        int i, j, k;
-        int count = 0;
-
-        i = left;
-        j = mid;
-        k = left;
-
-        // O(n)
-        while ((i <= mid - 1) && (j <= right)) {
-            if (arr[i] <= arr[j]) {
-                temp[k++] = arr[i++];
-            } else {
-                temp[k++] = arr[j++];
-                count = count + (mid - i);
+        // Use Bottom-Up approach - O(n^2)
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= maxcap; j++) {
+                // Starts with the bottom
+                if (i == 0 || j == 0) {
+                    knap[i][j] = 0;
+                } else if (wt[i - 1] <= j) {
+                    knap[i][j] = Math.max(val[i - 1] + knap[i - 1][j - wt[i - 1]], knap[i - 1][j]);
+                } else
+                    knap[i][j] = knap[i - 1][j];
             }
-
         }
 
-        // O(n)
-        while (i <= mid - 1)
-            temp[k++] = arr[i++];
-
-        // O(n)
-        while (j <= right)
-            temp[k++] = arr[j++];
-
-        // Copy elements to original array - O(n)
-        for (i = left; i <= right; i++)
-            arr[i] = temp[i];
-
-        return count;
-
+        return knap[n][maxcap];
     }
 
 }
