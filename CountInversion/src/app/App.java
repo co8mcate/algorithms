6e22c371b405f8e03555909c6f641592f128d3e6
co8mcate <37420994+co8mcate@users.noreply.github.com>
2020-04-18 21:06:09 -0700

    Project 3 is done

--- CountInversion/src/app/App.java
+++ CountInversion/src/app/App.java
@@ -3,103 +3,23 @@ package app;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
-import java.util.LinkedList;
-import java.util.Scanner;
-import java.util.HashMap;
+import java.util.*;
+import java.util.regex.Pattern;
 
-/*
-Objective 1: report if a cycle exists in the graph (there will either be 1 cycle or no cycles).
-Objective 2: Starting from Node 1, use BFS to tell me how many layers exist in the given graph.
-*/
-
-/*
-In a directed graph or cycle, each pair of consecutive nodes has
-the property that (v_i, v_i+1) is an edge. 
-*/
-
-/* 
-Let G be an undirected graph on n nodes.  Any two of the following
-statements implies the third:
-(i) G is connected
-(ii) G does not contain a cycle
-(iii) G has n - 1 edges
-*/
-
-/*
-A '0' means no connection to a node and '1' means connection to a node 
-*/
-
-// Node to store vertex and its parent info in BFS
-class Node {
-    int v, parent;
-
-    Node(int v, int parent) {
-        this.v = v;
-        this.parent = parent;
-    }
-};
+//Implement the O(n log n) algorithm that solves the Counting Inversions problem from Chapter 5.
 
 public class App {
-    // Perform BFS on graph starting from vertex src and
-    // returns true of cycle is found in the graph
-    public static boolean BFS(Map<Integer, ArrayList<Integer>> graph, int src, int N) {
-        // array to store level of each node
-        int level[] = new int[N];
-        // stores vertex is discovered or not
-        boolean[] discovered = new boolean[N];
-
-        // mark source vertex as discovered
-        discovered[src] = true;
-
-        // create a queue used to do BFS and
-        // push source vertex into the queue
-        Queue<Node> q = new ArrayDeque<>();
-        q.add(new Node(src, -1));
-
-        // run till queue is not empty
-        while (!q.isEmpty()) {
-            // pop front node from queue and print it
-            Node node = q.poll();
-
-            // do for every edge (v -> u)
-            for (int u : graph.get(node.v)) {
-                if (!discovered[u]) {
-
-                    // mark it discovered
-                    discovered[u] = true;
-
-                    // construct the queue node containing info
-                    // about vertex and push it into the queue
-                    q.add(new Node(u, node.v));
-                }
-
-                // u is discovered and u is not a parent
-                else if (u != node.parent) {
-                    // we found a cross-edge ie. cycle is found
-                    return true;
-                }
-            }
-        }
-
-        // No cross-edges found in the graph
-        return false;
-    }
 
     public static void main(final String[] args) throws FileNotFoundException {
 
         System.out.println("\n===================================\n");
-        System.out.println("\tGraph Representation\n");
+        System.out.println("\tCounting Inversions\n");
         System.out.println("===================================\n");
 
         try {
 
-            String filePath = "input1.txt";
+            String filePath = "input3.txt";
+            ArrayList<Integer> list = new ArrayList<>();
 
             // Create an Object for getting a file
             File myFile = new File(filePath);
@@ -108,63 +28,29 @@ public class App {
             // Validate that you have a file
             System.out.println("Found your file!");
 
-            System.out.println();
+            // Use a delimiter to read characters without whitespace
+            scan.useDelimiter(Pattern.compile(" "));
 
-            /*
-             * R will consists of nodes to which s has a path Initially R = {s} While there
-             * is an edge (u,v) where u in R and v is not in R Add v to R Endwhile
-             */
-
-            ArrayList<Integer> vertices = new ArrayList<>();
-            List<Integer> edges = new LinkedList();
-            Map<Integer, ArrayList<Integer>> graph = new HashMap();
-
-            ArrayList<ArrayList<Integer>> adjList = new ArrayList<ArrayList<Integer>>();
-
-            // Parse data and store in data structures for adjacency matrix - O(n^2)
+            // Parse and store data into ArrayList - O(n)
             while (scan.hasNextLine()) {
-                Scanner colReader = new Scanner(scan.nextLine());
-                ArrayList col = new ArrayList();
-                while (colReader.hasNextInt()) {
-                    col.add(colReader.nextInt());
-                }
-                adjList.add(col);
+                list.add(scan.nextInt());
             }
 
-            // Print a adjacency matrix of a graph representation - O(n)
-            System.out.println("Adjacency Matrix:");
-            for (ArrayList<Integer> rows : adjList) {
-                System.out.println(rows);
-            }
             System.out.println();
 
-            // Create an adjacency list for connections - O(n^2)
-            // row
-            for (int i = 0; i < adjList.size(); i++) {
-                // column
-                ArrayList<Integer> arr = new ArrayList<>();
-                for (int j = 0; j < adjList.get(i).size(); j++) {
-                    if (adjList.get(i).get(j) == 1) {
-                        arr.add(j);
-                    }
-                }
-                graph.put(i, arr);
-            }
+            System.out.println("A list of numbers: " + list);
 
-            // Print an adjacent list of the graph representation
-            System.out.println("Adjacency List:");
-            System.out.println(graph);
             System.out.println();
 
-            int numVertices = graph.size();
+            // Create a new array for mergeSort
+            int arr[] = new int[list.size()];
 
-            if (BFS(graph, 1, numVertices)) {
-                System.out.println("Does cycle exist? Yes");
-            } else
-                System.out.println("Does cycle exist? No");
+            for (int i = 0; i < list.size(); i++) {
+                arr[i] = list.get(i);
+            }
 
-            // call levels function with source as 0
-            printLevels(graph, numVertices, 0);
+            // Print a number of counting inversions
+            System.out.println("Number of inversions are " + mergeSort(arr));
 
             scan.close();
         } catch (IOException e) {
@@ -172,57 +58,71 @@ public class App {
         }
     }
 
-    // function to determine level of each node starting
-    // from x using BFS
-    static void printLevels(Map<Integer, ArrayList<Integer>> graph, int V, int x) {
-        // array to store level of each node
-        int level[] = new int[V];
-        boolean marked[] = new boolean[V];
+    // Driver method of MergeSort
+    static int mergeSort(int arr[]) {
 
-        // create a queue
-        Queue<Integer> que = new LinkedList<Integer>();
+        int numArray = arr.length;
 
-        // enqueue element x
-        que.add(x);
+        int temp[] = new int[numArray];
 
-        // initialize level of source node to 0
-        level[x] = 0;
+        return mergeSorts(arr, temp, 0, numArray - 1);
+    }
 
-        // marked it as visited
-        marked[x] = true;
+    // returns the number of inversions in the array - O(n log n)
+    static int mergeSorts(int arr[], int temp[], int left, int right) {
 
-        // do until queue is empty
-        while (que.size() > 0) {
+        int mid, count = 0;
 
-            // get the first element of queue
-            x = que.peek();
+        if (right > left) {
 
-            // dequeue element
-            que.remove();
+            // Divide and insert an integer as middle
+            mid = (right + left) / 2;
 
-            // traverse neighbors of node x
-            for (int i = 0; i < graph.get(x).size(); i++) {
-                // b is neighbor of node x
-                int b = graph.get(x).get(i);
+            count = mergeSorts(arr, temp, left, mid);
+            count += mergeSorts(arr, temp, mid + 1, right);
 
-                // if b is not marked already
-                if (!marked[b]) {
-
-                    // enqueue b in queue
-                    que.add(b);
-
-                    // level of b is level of x + 1
-                    level[b] = level[x] + 1;
-
-                    // mark b
-                    marked[b] = true;
-                }
-            }
+            // Merge two parts
+            count += merge(arr, temp, left, mid + 1, right);
         }
 
-        // display all nodes and their levels
-        System.out.println("Nodes" + " " + "Level");
-        for (int i = 0; i < V; i++)
-            System.out.println(" " + i + " --> " + level[i]);
+        return count;
     }
+
+    // This method merges two sorted arrays and returns inversion count in the
+    // arrays.
+    static int merge(int arr[], int temp[], int left, int mid, int right) {
+        int i, j, k;
+        int count = 0;
+
+        i = left;
+        j = mid;
+        k = left;
+
+        // O(n)
+        while ((i <= mid - 1) && (j <= right)) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+                count = count + (mid - i);
+            }
+
+        }
+
+        // O(n)
+        while (i <= mid - 1)
+            temp[k++] = arr[i++];
+
+        // O(n)
+        while (j <= right)
+            temp[k++] = arr[j++];
+
+        // Copy elements to original array - O(n)
+        for (i = left; i <= right; i++)
+            arr[i] = temp[i];
+
+        return count;
+
+    }
+
 }
